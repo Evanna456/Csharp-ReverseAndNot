bdfa108026866154b4c25306be65aa1fa45d7296
Evanna Tabs <steampunk852@yahoo.com>
2022-05-05 21:05:38 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Csharp_ReverseAndNot
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            program();
+        }
+        public static void program()
+        {
+            try
+            {
+                Console.WriteLine("Enter a number to reverse and not reverse");
+                int number = Int32.Parse(Console.ReadLine());
+                string string_number = number.ToString();
+                int length = string_number.Length;
+                string final_number = "";
+                for (int it = length - 1; it >= 0; it--)
+                {
+                    final_number += string_number[it];
+                }
+                final_number += string_number;
+                Console.WriteLine(final_number);
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("Wrong Input/s");
+                program();
+            }
+        }
+    }
+}
